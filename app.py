3e9130b479d07c40c66a87f11bd3711e3fb482fb
Khine Zaw <khinekzaw7@gmail.com>
2023-11-07 00:46:48 -0500

    new commit

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import stripe
+import os
+stripe_keys = {
+  'secret_key': os.environ['SECRET_KEY'],
+  'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
+
+stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
 
@@ -27,6 +35,30 @@ def index():
     else:
         # Render the template with no dataframe at the start
         return render_template('index.html')
+    
+@app.route('/')
+def index():
+    return render_template('index.html', key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in cents
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        source=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+    )
+
+    return render_template('charge.html', amount=amount)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
