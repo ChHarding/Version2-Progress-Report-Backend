a14447a9a69bb3b74094a71aa9043591beed189e
Khine Zaw <khinekzaw7@gmail.com>
2023-11-27 02:32:06 -0500

    fixing boostrap
    Picking up from last week.

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import stripe
 import os
-stripe_keys = {
-  'secret_key': os.environ['SECRET_KEY'],
-  'publishable_key': os.environ['PUBLISHABLE_KEY']
-}
-
-stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
 
--- app_CH.py
+++ app_CH.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import stripe
 import os
 
-from keys import SECRET_KEY, PUBLISHABLE_KEY
-
-#stripe.api_key = stripe_keys['secret_key']
-stripe.api_key = SECRET_KEY
 
 app = Flask(__name__, template_folder='templates',
                       static_folder='static')
@@ -33,11 +28,12 @@ def index():
         
         # Render the template with the filtered dataframe (sorted by rating and price)
         filtered_df = filtered_df.sort_values(['average_review_rating', 'price'], ascending=[False, True])
-        return render_template('index_CH.html', products=filtered_df.to_dict('records'), search=search_query, max_price=max_price, min_rating=min_rating)
+        return render_template('index.html', products=filtered_df.to_dict('records'), 
+                               search=search_query, max_price=max_price, min_rating=min_rating)
 
     else:
         # Render the template with no dataframe at the start
-        template =render_template('index_CH.html')
+        template =render_template('index.html')
         return template
 
 @app.route('/charge', methods=['POST'])
